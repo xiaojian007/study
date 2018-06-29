f6220dfbe0ffbd2926d1c5f436d3836551396930
xiaojian007 <386944072@qq.com>
2018-06-29 17:13:05 +0800

    banner

--- src/pages/home/banner/banner.vue
+++ src/pages/home/banner/banner.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="banner">
     <el-carousel :interval="5000" arrow="always" height="640px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <img class="banner-img" src="https://www.bitauto.com/topics/adtopic/region/dazhongChengduH5/images/header_3.jpg" alt="">
+      <el-carousel-item v-for="item in bannerData">
+        <a :href="item.targetUrl">
+          <img class="banner-img" :src="item.bannerImageUrl" :title="item.title" alt="">
+        </a>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -14,7 +16,33 @@
   export default {
     name: 'banner',
     data() {
-      return {}
+      return {
+        bannerData: [1, 2, 3, 4]
+      }
+    },
+    methods: {
+      obtain() {
+        var _this = this;
+        axios.get("api/banner/getPublishBannerList")
+          .then(function(response) {
+            var code = response.status;
+            if (code !== 200) {
+              _this.$message({
+                message: '错误',
+                type: 'error'
+              });
+            } else {
+              _this.bannerData = response.data.data
+              console.log(_this.bannerData)
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    },
+    mounted() {
+      this.obtain();
     }
   }
 </script>
